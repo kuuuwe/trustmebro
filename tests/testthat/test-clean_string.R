3c8e08e2486781e1f18e18c1526d7d0b97ce6398
Anwender <anneplaeschke@gmail.com>
2024-06-04 10:51:58 +0200

    demonstration für Tobias löschen

--- tests/testthat/test-clean_string.R
+++ tests/testthat/test-clean_string.R
@@ -1,5 +1,5 @@
 test_that("clean strings actually clenas a string", {
-  expect_equal(" 2B_Giraffen", "2B#GIRAFFEN")
-  expect_equal(" 1AFÜChse ", "1AFÜCHSE")
+  expect_equal(clean_string(" 2B_Giraffen"), "2B#GIRAFFEN")
+  expect_equal(clean_string(" 1AFuChse "), "1AFUCHSE")
   expect_error(2345)
 })
