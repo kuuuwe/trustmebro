7eff2f2227f61a5b7a303eb1d6c9abe35583b11d
Anwender <anneplaeschke@gmail.com>
2024-06-08 20:54:13 +0200

    08-06-24

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' testdata_department
+#'
+#' @description
+#' A simulated dataset, imitating data to be matched delivered from sources, in our work environment departments,
+#' where data was gathered.  
+#'
+#' @format ## 
+#' A tibble with 5 rows and 4 columns:
+#' \describe{
+#'   \item{schoolnumber}{School Identifier, should be a number}
+#'   \item{classname}{class designation of a student}
+#'   \item{sex}{sex}
+#'   \item{code}{SGIC of a student}
+#' }
+#' @source this dataset was simulated on the fly for package development and test purposes only. it does not contain real life data.
