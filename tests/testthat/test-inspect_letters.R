c6a66d488fdb4ffe521e89450844a417b36fb56c
Anwender <anneplaeschke@gmail.com>
2024-06-12 20:20:23 +0200

    invalid letters löschen

--- tests/testthat/test-inspect_letters.R
+++
@@ -1,4 +0,0 @@
-test_that("inspect_letters actually detects if a string contains the exact number of letters", {
-  expect_equal(inspect_letters("12345", 3), FALSE)
-  expect_equal(inspect_letters("ACAB5", 4), TRUE)
-})
