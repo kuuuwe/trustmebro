7fb333fd18f0306f4b73c488f108800268f334b8
Anwender <anneplaeschke@gmail.com>
2024-06-14 09:46:00 +0200

    names codestructure

--- tests/testthat/test-inpsect_codestructure.R
+++ tests/testthat/test-inpsect_codestructure.R
@@ -1,3 +1,3 @@
 test_that("inspect_codestructure actually identifies expected pattern", {
-  expect_equal(inspect_invalid_codestructure("ABC1234", "^[A-Za-z]{3}[0-9]{4}$"), TRUE)
+  expect_equal(inspect_codestructure("ABC1234", "^[A-Za-z]{3}[0-9]{4}$"), TRUE)
 })
