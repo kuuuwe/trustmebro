a3bba713a2fd0d52b5dd925a84b27e523f851835
Anwender <anneplaeschke@gmail.com>
2024-07-15 13:59:25 +0200

    inspect schoolnumber handles non-numeric inputs

--- R/inpsect_schoolnumber.R
+++ R/inpsect_schoolnumber.R
@@ -10,12 +10,14 @@
 #' inspect_schoolnumber(12345, 5)  # TRUE
 #' inspect_schoolnumber(123456, 5)  # FALSE
 inspect_schoolnumber <- function(schoolnumber, expected_length) {
-  # is schoolnumber a number?
-  if (!is.numeric(schoolnumber)) {
-    stop("invalid input: schoolnumber must be a numeric vector")
-  }
-    # check if length is as expected
+  # check if schoolnumber is numeric
+  is_numeric <- grepl("^[0-9]+$", as.character(schoolnumber))
+  
+  # check if length is as expected
   length_correct <- nchar(as.character(schoolnumber)) == expected_length
   
-  return(length_correct)
+  # return TRUE if both conditions are met, otherwise FALSE
+  valid <- is_numeric & length_correct
+  
+  return(valid)
 }
