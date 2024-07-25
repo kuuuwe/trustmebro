a43c410f04e8af17f6bf74e6c9e057eba4949c79
Anwender <anneplaeschke@gmail.com>
2024-07-25 11:14:30 +0200

    ß in replace alnum in df

--- R/replace_nonalnum_in_df.R
+++ R/replace_nonalnum_in_df.R
@@ -1,4 +1,4 @@
-#' Replace non-alphanumeric characters and remove spaces and tabs in character columns of a tibble or data frame, umlauts remain unchanged
+#' Replace non-alphanumeric characters and remove spaces and tabs in character columns of a tibble or data frame, umlauts and eszett remain unchanged
 #'
 #' @param data A dataframe or tibble containing columns to be processed
 #' @param replacement The character or string to replace non-alphanumeric characters with. Default is "#"
@@ -20,7 +20,7 @@ replace_nonalnum_in_df <- function(data, replacement = "#") {
   data <- data %>%
     mutate(across(where(is.character), ~ {
       .x <- gsub("[[:space:]]", "", .x)  #remove whitespace
-      .x <- gsub("[^[:alnum:]\u00e4\u00f6\u00fc\u00c4\u00d6\u00dc]", replacement, .x, perl = TRUE)  #replace nonalnum
+      .x <- gsub("[^[:alnum:]\u00e4\u00f6\u00fc\u00c4\u00d6\u00dc\u00df\u1e9e]", replacement, .x, perl = TRUE)  #replace nonalnum
       .x
     }))
   
