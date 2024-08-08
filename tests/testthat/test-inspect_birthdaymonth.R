8373c37637793ecd107e8e7e89b004564409f14b
Anwender <anneplaeschke@gmail.com>
2024-08-08 20:27:56 +0200

    first vingette, modified inspect_birthdaymotnh and report invalid birthdaymonth

--- tests/testthat/test-inspect_birthdaymonth.R
+++ tests/testthat/test-inspect_birthdaymonth.R
@@ -1,6 +1,7 @@
 test_that("inspect_birthdaymonth detects if birthday and birthmonth are correctly provided", {
-  expect_equal(inspect_birthdaymonth("KNE1612", 4, 7), TRUE)
-  expect_equal(inspect_birthdaymonth("ALS3102", 4, 7), FALSE)
-  expect_error(inspect_birthdaymonth("AAA0101", 7, 2))
-  expect_error(inspect_birthdaymonth(345, 1, 7))
+  expect_equal(inspect_birthdaymonth("KNE1612"), TRUE)
+  expect_equal(inspect_birthdaymonth("ALS3102"), FALSE)
+  expect_equal(inspect_birthdaymonth("5AAA0101"), FALSE)
+  expect_equal(inspect_birthdaymonth("3103AAA"), TRUE)
+  expect_error(inspect_birthdaymonth(345))
 })
