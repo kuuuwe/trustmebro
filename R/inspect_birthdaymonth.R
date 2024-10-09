321fcb04d4604f3be94673744db232221e6ead4f
Anwender <anneplaeschke@gmail.com>
2024-10-09 13:39:50 +0200

    format options added

--- R/inspect_birthdaymonth.R
+++ R/inspect_birthdaymonth.R
@@ -1,10 +1,12 @@
 #' inspect birthday and -month component of a string
 #'
 #'@description
-#'This function checks if a string contains only one number-component representing a valid combination of birthday followed by month
+#'This function checks if a string contains only one number-component representing a valid combination of birthday and birthmonth
 #'
 #' @param code a string containing the SGIC
-#'
+#' @param format A string specifying the format of the birthdate-components in code. 
+#'        Use "DDMM" for day-month format and "MMDD" for month-day format. 
+#'        Default is "DDMM".
 #' @return a logical value `TRUE` if the string hat only one number-component representing a valid birthday and -month, otherwise `FALSE`. 
 #' @export
 #'
@@ -12,11 +14,16 @@
 #' inspect_birthdaymonth("DEF2802") # TRUE
 #' inspect_birthdaymonth("GHI3002") # FALSE
 #' 
-inspect_birthdaymonth <- function(code) {
+inspect_birthdaymonth <- function(code, format = "DDMM") {
   
   # check if input is a character
   if (!is.character(code)) {
     stop("invalid input: code must be a character vector")
+  }
+  
+  # check if format is valid
+  if (!format %in% c("DDMM", "MMDD")) {
+    stop("invalid format: format must be 'DDMM' or 'MMDD'")
   }
   
   # function for checking birthday and birthmonth
@@ -32,9 +39,15 @@ inspect_birthdaymonth <- function(code) {
     # extract number component
     date_part <- match
     
-    # extract day and month
-    day <- as.numeric(substr(date_part, 1, 2))
-    month <- as.numeric(substr(date_part, 3, 4))
+    if (format == "DDMM") {
+      # DDMM: first 2 digits = day, last 2 digits = month
+      day <- as.numeric(substr(date_part, 1, 2))
+      month <- as.numeric(substr(date_part, 3, 4))
+    } else if (format == "MMDD") {
+      # MMDD: first 2 digits = month, last 2 digits = day
+      month <- as.numeric(substr(date_part, 1, 2))
+      day <- as.numeric(substr(date_part, 3, 4))
+    }
     
     # check if month is valid
     if (is.na(month) || month < 1 || month > 12) {
